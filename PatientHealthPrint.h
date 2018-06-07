d0f2a1b7260ad1d4f8ad6ee00d3fbe2c9cd7d4bd
BorghinoEmilie <emilie.borghino@gmail.com>
2018-06-07 19:01:31 +0200

    Commentaire <PatientHealthPrint.h>

--- PatientHealthPrint.h
+++ PatientHealthPrint.h
@@ -19,8 +19,7 @@
 
 //------------------------------------------------------------------------
 // Rôle de la classe <PatientHealthPrint>
-//
-//
+//  Cette classe représente les empreintes patients
 //------------------------------------------------------------------------
 
 class PatientHealthPrint : public HealthPrint
@@ -29,43 +28,44 @@ class PatientHealthPrint : public HealthPrint
 
 public:
 //----------------------------------------------------- Méthodes publiques
-    // type Méthode ( liste des paramètres );
     // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
+    //  Renvoie la liste des noms de maladie auxquelles l'empreinte correspond
+    // et a chacun est associé le pourcentage de correspondance
     map<string, double> getPatientDiseases(); 
     
+    // Mode d'emploi :
+    //  Affiche l'empreinte patient sur le flux stream (console par défaut)
     void display(ostream & stream = std::cout);
 
+    // Mode d'emploi :
+    //  Ajoute le nom de la maladie name à la liste des maladies associées
+    //  à l'empreinte et lui défini value en pourcentage de correspondance
+    // Contrat :
+    //  name doit être le nom d'une maladie gérée par le système
+    //  value doit être un pourcentage (0 <= value <= 1)
     void setDiseasePercent(string name, double value);
 //------------------------------------------------- Surcharge d'opérateurs
     PatientHealthPrint & operator = ( const PatientHealthPrint & unPatientHealthPrint );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
 
 //-------------------------------------------- Constructeurs - destructeur
-    PatientHealthPrint ( string line, vector<string> & labelOrder );
     // Mode d'emploi :
-    //
+    //  Créé une empreinte patient avec line une ligne du fichier d'empreintes
+    // patients et labelOrder spécifiant l'ordre des données dans line
     // Contrat :
-    //
+    //  line doit respecté le format CSV et venir du fichier d'empreintes patients
+    //  et doit respecter l'ordre des données spécifié par labelOrder
+    PatientHealthPrint ( string line, vector<string> & labelOrder );
 
     virtual ~PatientHealthPrint ( );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
+
 
 //------------------------------------------------------------------ PRIVE
 private: 
 //----------------------------------------------------- Méthodes privées
 
-//----------------------------------------------------- Attributs privés   
+//----------------------------------------------------- Attributs privés
+    // liste des noms de maladie auxquelles l'empreinte correspond
+    // et a chacun est associé le pourcentage de correspondance
     map<string, double> diseases;
 protected:
 //----------------------------------------------------- Méthodes protégées
